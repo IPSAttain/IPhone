e5fd9b7b449dbd4a9d8a622df5161ab3a1cdcdd3
IPSAttain <35669489+IPSAttain@users.noreply.github.com>
2022-07-08 08:04:35 +0200

    New System Requirements for ICloud (#5)
    * Style Check
    
    * New System Requirements for ICloud

--- FindMyiPhoneConfig/module.php
+++ FindMyiPhoneConfig/module.php
@@ -1,96 +1,90 @@
 <?php
-	require_once __DIR__ . '/../libs/FindMyiPhone.php';
-	class FindMyiPhoneConfig extends IPSModule {
+    require_once __DIR__ . '/../libs/FindMyiPhone.php';
+    class FindMyiPhoneConfig extends IPSModule
+    {
+        public function Create()
+        {
+            //Never delete this line!
+            parent::Create();
+            $this->RequireParent("{A5FAE93E-34F2-2F0E-8D5C-929BCD5CF5B7}");
+        }
 
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
-			$this->RequireParent("{A5FAE93E-34F2-2F0E-8D5C-929BCD5CF5B7}");
-		}
+        public function Destroy()
+        {
+            //Never delete this line!
+            parent::Destroy();
+        }
 
-		public function Destroy()
-		{
-			//Never delete this line! 
-			parent::Destroy();
-		}
+        public function ApplyChanges()
+        {
+            //Never delete this line!
+            parent::ApplyChanges();
+        }
 
-		public function ApplyChanges()
-		{
-			//Never delete this line!
-			parent::ApplyChanges();
-		}
+        public function GetConfigurationForm()
+        {
+            $Values = json_decode($this->GetFormData());
+            $this->SendDebug("Elements", json_encode($Values), 0);
+            $form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
+            $form['actions'][0]['values'] = $Values;
+            return json_encode($form);
+        }
+        
+        private function GetFormData()
+        {
+            $devices = $this->SendData();
+            
 
-		public function GetConfigurationForm() 
-		{
-			$Values = json_decode($this->GetFormData());	
-			$this->SendDebug("Elements", json_encode($Values), 0);
-			$form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
-			$form['actions'][0]['values'] = $Values;
-			return json_encode($form);
-		}
-		
-		private function GetFormData()
-		{
-			$devices = $this->SendData();
-			
+            $guid = "{7B500376-2990-711D-7B4D-6D7D47351D73}";
+            $Instances = IPS_GetInstanceListByModuleID($guid);
+            
+            // Configurator
+            $Values = array();
+            $devices = json_decode($devices, true);
+            //$this->LogMessage(__FUNCTION__.print_r($devices,true) , 10206);
+            if ($devices == "Empty User or Password") {
+                $this->SendDebug(__FUNCTION__, " Empty User or Password", 0);
+                return;
+            } elseif ($devices == "wrong credentials" || $devices == "") {
+                $this->SendDebug(__FUNCTION__, " No feedback from iCloud Server. Maybe wrong User or Password", 0);
+                return;
+            } else {
+                foreach ($devices as $device) {
+                    $ID	= 0;
+                    foreach ($Instances as $Instance) {
+                        //$this->SendDebug("Created Instances", IPS_GetObject($Instance)['ObjectName'] , 0);
+                        if (IPS_GetProperty($Instance, 'DeviceID')== $device['id']) {
+                            $ID = $Instance;
+                        }
+                    }
+                    $Values[] = [
+                    'instanceID' => $ID,
+                    'name'       => $device['name'],
+                    'DeviceID'   => $device['id'],
+                    'modelID'    => $device['deviceDisplayName'],
+                    'DetailType' => $device['modelDisplayName'],
+                    'create'	 =>
+                    [
+                        "moduleID"      => "{7B500376-2990-711D-7B4D-6D7D47351D73}",
+                        "configuration" => [
+                            "DeviceID"  => $device['id'],
+                            "DeviceName" => $device['name']
+                        ]
+                    ]
+                ];
+                }
+                return json_encode($Values);
+            }
+        }
 
-			$guid = "{7B500376-2990-711D-7B4D-6D7D47351D73}";
-			$Instances = IPS_GetInstanceListByModuleID($guid);
-			
-			// Configurator
-			$Values = array();
-			$devices = json_decode($devices, true);
-			//$this->LogMessage(__FUNCTION__.print_r($devices,true) , 10206);
-			if ($devices == "Empty User or Password") 
-			{
-				$this->SendDebug(__FUNCTION__ , " Empty User or Password",0);
-				return;
-			}
-			elseif ($devices == "wrong credentials" || $devices == "") 
-			{
-				$this->SendDebug(__FUNCTION__ , " No feedback from iCloud Server. Maybe wrong User or Password" , 0);
-				return;
-			}
-			else
-			{
-			foreach ($devices as $device)
-			{
-				$ID	= 0;
-				foreach ($Instances as $Instance){
-					//$this->SendDebug("Created Instances", IPS_GetObject($Instance)['ObjectName'] , 0);
-					if (IPS_GetProperty($Instance,'DeviceID')== $device['id'])
-					{
-						$ID = $Instance;
-					}
-				}
-				$Values[] = [
-					'instanceID' => $ID,
-					'name'       => $device['name'],
-					'DeviceID'   => $device['id'],
-					'modelID'    => $device['deviceDisplayName'],
-					'DetailType' => $device['modelDisplayName'],
-					'create'	 => 
-					[
-						"moduleID"      => "{7B500376-2990-711D-7B4D-6D7D47351D73}",
-						"configuration" => [
-							"DeviceID"  => $device['id'],
-							"DeviceName" => $device['name']
-						]
-					]
-				];
-			}
-			return json_encode($Values);
-		}
-		}
-
-		protected function SendData() {
-			$return = $this->SendDataToParent(json_encode([
-				'DataID' => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}",
-				'Buffer' => utf8_encode("Get_Data"),
-			]));
-			$this->SendDebug("Received from Gateway", $return , 0);
-			//$this->LogMessage(__FUNCTION__, $return , 10206);
-			return $return;
-		}
-	}
+        protected function SendData()
+        {
+            $return = $this->SendDataToParent(json_encode([
+                'DataID' => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}",
+                'Buffer' => utf8_encode("Get_Data"),
+            ]));
+            $this->SendDebug("Received from Gateway", $return, 0);
+            //$this->LogMessage(__FUNCTION__, $return , 10206);
+            return $return;
+        }
+    }
--- FindMyiPhoneGateway/module.php
+++ FindMyiPhoneGateway/module.php
@@ -1,99 +1,97 @@
 <?php
 require_once __DIR__ . '/../libs/FindMyiPhone.php';
-	class FindMyiPhoneGateway extends IPSModule {
+    class FindMyiPhoneGateway extends IPSModule
+    {
+        public function Create()
+        {
+            //Never delete this line!
+            parent::Create();
 
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
+            $this->RegisterPropertyString("User", "user@domain.tld");
+            $this->RegisterPropertyString("Password", "");
+            $this->RegisterPropertyInteger("Refresh", 5);
+            $this->RegisterTimer("Update", 300000, "FMiP_UpdateData($this->InstanceID);");
+        }
 
-			$this->RegisterPropertyString("User", "user@domain.tld");
-			$this->RegisterPropertyString("Password", "");
-			$this->RegisterPropertyInteger("Refresh",5);
-			$this->RegisterTimer("Update", 300000, "FMiP_UpdateData($this->InstanceID);");
-		}
+        public function Destroy()
+        {
+            //Never delete this line!
+            parent::Destroy();
+        }
 
-		public function Destroy()
-		{
-			//Never delete this line!
-			parent::Destroy();
-		}
+        public function ApplyChanges()
+        {
+            //Never delete this line!
+            parent::ApplyChanges();
+            $this->SetTimerInterval("Update", $this->ReadPropertyInteger("Refresh")*60000);
+        }
 
-		public function ApplyChanges()
-		{
-			//Never delete this line!
-			parent::ApplyChanges();
-			$this->SetTimerInterval("Update", $this->ReadPropertyInteger("Refresh")*60000);
-		}
+        public function ForwardData($JSONString)
+        {
+            $data = json_decode($JSONString, true);
+            //$this->LogMessage(__FUNCTION__. utf8_decode($data->Buffer) , 10206);
+            $data = preg_split('/\n|\r\n?/', $data['Buffer']);
+            //$this->LogMessage(__FUNCTION__. print_r($data) , 10206) ;
+            $User = $this->ReadPropertyString("User");
+            $Password = $this->ReadPropertyString("Password");
+            $returndata = "";
+            switch ($data[0]) {
+                case 'Get_Data':
+                    $returndata = $this->GetData();
+                    break;
 
-		public function ForwardData($JSONString)
-		{
-			$data = json_decode($JSONString,true);
-			//$this->LogMessage(__FUNCTION__. utf8_decode($data->Buffer) , 10206);
-			$data = preg_split('/\n|\r\n?/', $data['Buffer']);
-			//$this->LogMessage(__FUNCTION__. print_r($data) , 10206) ;
-			$User = $this->ReadPropertyString("User");
-			$Password = $this->ReadPropertyString("Password");
-			$returndata = "";
-			switch ($data[0])
-			{
-				case 'Get_Data':
-					$returndata = $this->GetData();
-					break;
+                case 'Update_Device_Data':
+                        $returndata = $this->UpdateData();
+                        break;
 
-				case 'Update_Device_Data':
-						$returndata = $this->UpdateData();
-						break;
+                case 'Play_Sound':
+                    $FindMyiPhone = new FindMyiPhone($User, $Password);
+                    $returndata = ($FindMyiPhone->play_sound($data[1])->statusCode == 200) ? 'Sent!' : 'Failed!';
+                    break;
 
-				case 'Play_Sound':
-					$FindMyiPhone = new FindMyiPhone($User, $Password);  
-					$returndata = ($FindMyiPhone->play_sound($data[1])->statusCode == 200) ? 'Sent!' : 'Failed!';
-					break;
+                case 'Send_Message':
+                    $FindMyiPhone = new FindMyiPhone($User, $Password);
+                    $returndata = ($FindMyiPhone->send_message($data[1], $data[2], false, 'Important Message')->statusCode == 200) ? 'Sent!' : 'Failed!';
+                    break;
+                
+                case 'Lost_Device':
+                    $FindMyiPhone = new FindMyiPhone($User, $Password);
+                    //$data[2] => passcode ( 4 digits)
+                    //$data[3] => owner_phone_number
+                    //$data[4] => sound (true/false)
+                    //$data[5] => message
+                    $returndata = ($FindMyiPhone->lost_device($data[1], $data[2], $data[3], true, $data[5])->statusCode == 200) ? 'Sent!' : 'Failed!';
+                    break;
 
-				case 'Send_Message':
-					$FindMyiPhone = new FindMyiPhone($User, $Password);
-					$returndata = ($FindMyiPhone->send_message($data[1], $data[2], false, 'Important Message')->statusCode == 200) ? 'Sent!' : 'Failed!';
-					break;
-				
-				case 'Lost_Device':
-					$FindMyiPhone = new FindMyiPhone($User, $Password);
-					//$data[2] => passcode ( 4 digits)
-					//$data[3] => owner_phone_number
-					//$data[4] => sound (true/false)
-					//$data[5] => message
-					$returndata = ($FindMyiPhone->lost_device($data[1], $data[2], $data[3], true, $data[5])->statusCode == 200) ? 'Sent!' : 'Failed!';
-					break;
+                case 'Phone_Found':
+                    $FindMyiPhone = new FindMyiPhone($User, $Password);
+                    $returndata = ($FindMyiPhone->notify_when_found($data[1])->statusCode == 200) ? 'Sent!' : 'Failed!';
+                    break;
+            }
+            return $returndata;
+        }
+        
+        public function UpdateData()
+        {
+            $this->SendDebug(__FUNCTION__, "Data Update requested ", 0);
+            $phonedata = $this->GetData();
+            $this->SendDataToChildren(json_encode(array("DataID" => "{018EF6B5-AB94-40C6-AA53-46943E824ACF}", "Buffer" => $phonedata)));
+        }
 
-				case 'Phone_Found':
-					$FindMyiPhone = new FindMyiPhone($User, $Password);  
-					$returndata = ($FindMyiPhone->notify_when_found($data[1])->statusCode == 200) ? 'Sent!' : 'Failed!';
-					break;
-			}
-			return $returndata;
-		}
-		
-		public function UpdateData()
-		{
-			$this->SendDebug(__FUNCTION__, "Data Update requested " , 0);
-			$phonedata = $this->GetData();
-			$this->SendDataToChildren(json_encode(Array("DataID" => "{018EF6B5-AB94-40C6-AA53-46943E824ACF}", "Buffer" => $phonedata)));
-		}
-
-		protected function GetData()
-		{
-			$User = $this->ReadPropertyString("User");
-			$Password = $this->ReadPropertyString("Password");
-			if ($Password == "" || $User == "") 
-			{
-				$this->LogMessage(__FUNCTION__. ' Empty User or Password' ,10204);
-				$this->SendDebug("Config", "Empty User or Password" , 0);
-				return "Empty User or Password";
-			}
-			$this->SendDebug(__FUNCTION__, "User = $User " , 0);
-			$FindMyiPhone = new FindMyiPhone($User, $Password);
-			$devices = $FindMyiPhone->devices;
-			$devices = json_encode($devices);
-			$this->SendDebug(__FUNCTION__, print_r($devices,true) , 0);
-			return $devices;
-		}
-	}
+        protected function GetData()
+        {
+            $User = $this->ReadPropertyString("User");
+            $Password = $this->ReadPropertyString("Password");
+            if ($Password == "" || $User == "") {
+                $this->LogMessage(__FUNCTION__. ' Empty User or Password', 10204);
+                $this->SendDebug("Config", "Empty User or Password", 0);
+                return "Empty User or Password";
+            }
+            $this->SendDebug(__FUNCTION__, "User = $User ", 0);
+            $FindMyiPhone = new FindMyiPhone($User, $Password);
+            $devices = $FindMyiPhone->devices;
+            $devices = json_encode($devices);
+            $this->SendDebug(__FUNCTION__, print_r($devices, true), 0);
+            return $devices;
+        }
+    }
